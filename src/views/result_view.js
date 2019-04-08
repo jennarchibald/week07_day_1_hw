1f0dc4f6c6a613365618cdc17bdf7b83a39c2f91
jennarchibald <codeclan@codeclans-mbp.home>
2019-04-08 20:33:42 +0100

    adding number display

--- src/views/result_view.js
+++ src/views/result_view.js
@@ -6,16 +6,26 @@ const ResultView = function () {
 
 ResultView.prototype.bindEvents = function () {
 
+  PubSub.subscribe('FormView: number-submitted', (event) => {
+    this.displayNumber(event.detail);
+  });
+
   PubSub.subscribe('PrimeChecker: result', (event)=> {
     this.displayResult(event.detail);
   });
 };
 
 
+ResultView.prototype.displayNumber = function (number) {
+  const numberChecked = document.querySelector('#number-checked');
+  numberChecked.textContent = number + `...?`;
+};
+
 ResultView.prototype.displayResult = function (answer) {
   const result = document.querySelector('#result');
   if (answer) {
     result.textContent = `Yes! It's Prime!`;
+    console.log(result.textContent);
   } else {
     result.textContent = `No... It's not prime...`;
   }
