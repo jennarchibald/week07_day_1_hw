77543c10500145b93b3de17576d0cd50ff9f6861
jennarchibald <codeclan@CodeClans-MacBook-Pro.local>
2019-04-09 09:06:59 +0100

    removed console log

--- src/models/prime_checker.js
+++ src/models/prime_checker.js
@@ -16,12 +16,12 @@ PrimeChecker.prototype.bindEvents = function(){
 PrimeChecker.prototype.checkPrime = function (number) {
   if (number == 2){ return true};
 
-  if (number <= 1 || number % 2 == 0 ){return false};
+  if (number <= 1 || number % 2 === 0 ){return false};
 
-  let largestPossibleFactor = (number - 1) / 2;
+  let largestPossibleFactor = Math.floor(Math.sqrt(number));
 
   for (let i = 2; i <= largestPossibleFactor; i++){
-    if (number % i == 0){
+    if (number % i === 0){
       return false;
     };
   };
